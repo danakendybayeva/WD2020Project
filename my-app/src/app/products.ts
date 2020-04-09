847d9e14eda3114544a8afa868ca45c12c132046
danakendybayeva <danka.kent@gmail.com>
2020-04-10 03:13:58 +0600

    project

---
+++ my-app/src/app/products.ts
@@ -0,0 +1,17 @@
+export interface Product{
+    id: number;
+    image: string;
+    name: string;
+    author: string;
+    description: string;
+    price: string;
+    rating: number;
+    year: number;
+    genre: number;
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
