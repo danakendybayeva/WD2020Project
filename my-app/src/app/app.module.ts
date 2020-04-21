d26273ad468799ebf7d5d23a736ecd21cec8dc91
danakendybayeva <danka.kent@gmail.com>
2020-04-22 02:21:28 +0600

    comment

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
-// import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 // import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
